6b2a32aa5ae01c6ec2cd065d7f92cfc14f1dc24f
manzil-infinity180 <rahulvs2809@gmail.com>
2024-03-03 12:16:03 +0530

    node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({path:'./config.env'});
 const mongoose = require("mongoose");
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const app = require("./app");
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.PASSWORD);
 
