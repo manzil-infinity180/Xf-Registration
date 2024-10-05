b4b26b9a8df44f3cf1307434add7045a156f9b79
Rahul Vishwakarma <rahulvs2809@gmail.com>
2024-10-06 01:06:23 +0530

    Added origin for dev

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["https://xf-frontend.onrender.com"],
+  origin: ["https://xf-frontend.onrender.com","*","http://localhost:5173"],
   // origin: ["http://localhost:5173"],
   methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"], 
   credentials:true
